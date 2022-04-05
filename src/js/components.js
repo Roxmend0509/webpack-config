7bbb419c81936cbd775fe6ff52bf7177df0c7ef7
Roxana <rox050996@gmail.com>
2022-04-04 22:51:31 -0500

    config webpack

---
+++ src/js/components.js
@@ -0,0 +1,20 @@
+import '../css/components.css';
+// import webpacklogo from '../assets/img/webpack-logo.png';
+
+export const saludar = (nombre)=>{
+    console.log('Creando H1');
+
+    const h1=document.createElement('h1');
+
+    h1.innerText = `Hola, ${nombre}`
+
+    document.body.append(h1);
+
+
+    //IMG
+
+    // const img=document.createElement('img');
+
+    // img.src=webpacklogo;
+    // document.body.append(img);
+}
---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+    mode: 'development',
+
+    output: {
+        clean:true
+      },
+
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                loader: "html-loader",
+                options:{
+                    sources:false
+                }
+            },
+            {
+                test: /\.css$/,
+                exclude: /style.css$/,
+                use: ['style-loader','css-loader']
+            },
+            {
+                test: /style.css$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/,
+                loader: 'file-loader'
+              },
+              {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: "babel-loader",
+                  options: {
+                    presets: ['@babel/preset-env']
+                  }
+                }
+              }
+        ]
+    },
+
+    optimization: {},
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Webpack App',
+            template: 'src/index.html'
+        }),
+
+        new MiniCssExtractPlugin({
+            filename:'[name].[fullhash].css',
+            ignoreOrder:false
+        }),
+        new CopyPlugin({
+            patterns: [
+              { from: "src/assets/", to: "assets/" }
+            ],
+          }),
+    ],
+}
